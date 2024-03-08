df7d151049be434b98bf493d4b0c1f151cff2aca
Sekionn <Sjknudesn@gmail.com>
2024-03-08 09:41:23 +0100

    revert images

--- src/App.tsx
+++ src/App.tsx
@@ -21,12 +21,12 @@ export class App extends Component<IProps, IState> {
 
   sliderGrabbed = false;
   areas: Array<PreviewObject> = [
-    { name: "Again", img: "Portfolio/Images/ProjectImages/again.png", key: "again", downloadKey: "Again.zip" },
-    { name: "Boss Slayers", img: "Portfolio/Images/ProjectImages/bossSlayers.png", key: "bossSlayers", downloadKey: "BossSlayers.zip" },
-    { name: "Escape", img: "Portfolio/Images/ProjectImages/escape.png", key: "escape", downloadKey: "Escape.zip" },
-    { name: "Introvert Shopping", img: "Portfolio/Images/ProjectImages/introShop.png", key: "introShop", downloadKey: "IntrovertShopping.zip" },
-    { name: "School", img: "Portfolio/Images/ProjectImages/school.png", key: "school", downloadKey: "School.zip" },
-    { name: "Silk Route", img: "Portfolio/Images/ProjectImages/silkroutegame.png", key: "silkroutegame", downloadKey: "Silkroute.zip" }];
+    { name: "Again", img: "./Images/ProjectImages/again.png", key: "again", downloadKey: "Again.zip" },
+    { name: "Boss Slayers", img: "./Images/ProjectImages/bossSlayers.png", key: "bossSlayers", downloadKey: "BossSlayers.zip" },
+    { name: "Escape", img: "./Images/ProjectImages/escape.png", key: "escape", downloadKey: "Escape.zip" },
+    { name: "Introvert Shopping", img: "./Images/ProjectImages/introShop.png", key: "introShop", downloadKey: "IntrovertShopping.zip" },
+    { name: "School", img: "./Images/ProjectImages/school.png", key: "school", downloadKey: "School.zip" },
+    { name: "Silk Route", img: "./Images/ProjectImages/silkroutegame.png", key: "silkroutegame", downloadKey: "Silkroute.zip" }];
 
   projectSelected?: PreviewObject;
   dragging: boolean = false;
